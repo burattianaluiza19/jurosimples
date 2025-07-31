309aef504a270f8c25e025028b42e8b2e7623a0a
burattianaluiza19 <ana.buratti.barbosa@escola.pr.gov.br>
2025-07-31 12:10:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function anajuro(){
+    let capital = parseInt(document. getElementById ('capital').value);
+
+    let indice = parseInt(document. getElementById ('índice').value);
+
+    let tempo = parseInt(document. getElementById ('tempo').value);
+    índice=índice/100
+
+    let juros=capital*índice*tempo;
+    let montante=capital+ juros;
+    document.getElementById('montante').textContent= 'montante:' + montante;
+    document.getElementById('juros').textContent= 'juros:' + juros;
+}
